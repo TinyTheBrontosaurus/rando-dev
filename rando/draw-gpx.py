045ef21df9d82058ced1bc9b25c978bc52168d63
Eric Nelson <big89e@gmail.com>
2022-07-03 18:42:31 -0400

    Plotting each aid to aid station elevation

--- rando/draw-gpx.py
+++ rando/draw-gpx.py
@@ -4,6 +4,9 @@ import gpxpy.gpx
 import pandas as pd
 from rando import definitions
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy import spatial
+from natsort import natsorted
 
 
 def main(_argv):
@@ -15,8 +18,60 @@ def main(_argv):
     lons = [pt.longitude for pt in gpx.tracks[0].segments[0].points]
     eles = [pt.elevation * 39./12 for pt in gpx.tracks[0].segments[0].points]
 
-    plt.plot(eles)
+    wplats = [pt.latitude for pt in gpx.waypoints]
+    wplons = [pt.longitude for pt in gpx.waypoints]
+    wpnams = [pt.comment for pt in gpx.waypoints]
+
+    nplats = np.array(lats)
+    nplons = np.array(lons)
+
+    A = np.stack((nplats, nplons)).transpose()
+
+    wpdims = []
+    for wplat, wplon, wpnam in zip(wplats, wplons, wpnams):
+        pt = (wplat, wplon)
+        _distance, idx = spatial.KDTree(A).query(pt)
+        wpdims.append(idx)
+
+    wpdims, wpnams = parallel_sort(wpdims, wpnams)
+
+    last = 0
+    last_title = "start"
+    count = 0
+    for dim, title in zip(wpdims, wpnams):
+        plt.figure()
+        local_ele = eles[last:dim]
+        ax = plt.plot(local_ele)
+        plt.title(f"{last_title} to {title}")
+        plt.xlabel("")
+        plt.ylabel("Elevation (ft)")
+        plt.ylim([500, 2200])
+        frame1 = plt.gca()
+        frame1.axes.xaxis.set_ticklabels([])
+        plt.tick_params(
+            axis='x',  # changes apply to the x-axis
+            which='both',  # both major and minor ticks are affected
+            bottom=False,  # ticks along the bottom edge are off
+            top=False,  # ticks along the top edge are off
+            labelbottom=False)  # labels along the bottom edge are off
+        last = dim
+        last_title = title
+        count += 1
+        if count > 1:
+            break
+
     plt.show()
+
+    pass
+
+def parallel_sort(X, Y):
+    x_sorted = [x for _, x in natsorted(zip(Y, X))]
+    y_sorted = natsorted(Y)
+    return x_sorted, y_sorted
+
+
+#    plt.plot(eles)
+#    plt.show()
     pass
 
 
